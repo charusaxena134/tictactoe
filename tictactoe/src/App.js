f3798f6c8cc0e9ab024bdc483d607b5cd4081555
charusaxena134 <charusaxena134@gmail.com>
2021-07-29 19:38:19 +0530

    styled the entired app

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -45,11 +45,17 @@ const App = () => {
   }
   return (
     <div className = "app">
-      <h1>TIC TAC TOE</h1>
+      <h1>TIC <span className="text-green">TAC</span> TOE</h1>
       <StatusMessage winner={winner} current = {current}/>
       <Board board = {current.board} handleSquareClick ={handleSquareClick} winningSquares ={winningSquares}/>
-      <button type = "button" onClick = {onNewGame}>Start the new game</button>
+      <button type = "button" 
+        onClick = {onNewGame} 
+        className = {`btn-reset ${winner? 'active' :'' }`}>
+        Start the new game
+      </button>
+      <h2 style = {{fontWeight: 'normal'}}>Current game history</h2>
       <History history ={history} moveTo ={moveTo} currentMove ={currentMove}/>
+      <div class = "bg-balls" />
     </div>
   );
 };
--- tictactoe/src/components/StatusMessage.js
+++ tictactoe/src/components/StatusMessage.js
@@ -3,14 +3,28 @@ import React from 'react'
 const StatusMessage = ({winner, current}) => {
   const noMoveLeft = current.board.every(el => el !== null) 
   return (
-     <h2>
-       {winner && `winner is ${winner}`}
-       {!winner && 
-         !noMoveLeft && 
-         `Next player is ${current.isXNext ? 'X' : 'O'}` 
+     <div className = "status-mesaage">
+       {winner && 
+       <>
+         Winner is {' '}
+         <span className ={winner === 'X' ? 'text-green' : 'text-orange'}>
+           {winner}
+       </span>
+       </>
        }
-       {!winner && noMoveLeft && 'X and 0 tied'}
-     </h2>
+       {!winner && !noMoveLeft && 
+         <>
+           Next player is {' '}
+           <span className = {current.isXNext ? 'text-green' : 'text-orange'}>
+             {current.isXNext ? 'X' : 'O'}{' '}
+           </span>
+         </>
+       }
+       {!winner && noMoveLeft && <>
+         <span className = "text-green">X</span> <span class = "text-orange">O</span> tied
+       </>
+       }
+     </div>
   );
 };
 
